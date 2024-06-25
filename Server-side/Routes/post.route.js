6a760cf6cd2d95ed18bc9b322b501fe4a18f694b
satvik297 <satvikkakkar5@gmail.com>
2024-06-25 19:26:21 +0530

    create getpost api route

--- Server-side/Routes/post.route.js
+++ Server-side/Routes/post.route.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js';
-import { create } from '../controllers/post.controller.js';
+import { create ,getposts } from '../controllers/post.controller.js';
 
 const router  = express.Router();
 
 
 router.post('/create' ,verifyToken, create)
+//anyone without verifying can see the post therefore we dont need the verification token
+
+router.get('/getposts' , getposts)
 
 
 export default router;
