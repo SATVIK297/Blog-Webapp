6583647517f705d64e9ed31adb996fcedfb8b255
satvik297 <satvikkakkar5@gmail.com>
2024-06-26 17:15:41 +0530

    created get users api route

--- Server-side/Routes/user.route.js
+++ Server-side/Routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { deleteUser, test, updateUser,signOut } from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser,signOut,getUsers } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router  = express.Router();
@@ -13,5 +13,6 @@ router.get('/test' , test)
 router.put('/update/:userId' ,verifyToken, updateUser)
 router.delete('/delete/:userId' ,verifyToken, deleteUser)
 router.post('/signout' , signOut)
+router.get('/getusers',verifyToken,getUsers)
 
 export default router
